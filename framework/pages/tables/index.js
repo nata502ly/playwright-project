1e21b2eaef6a5c72159d8e07235ee9936088d89e
nataliaroshchyna <natalia.roshchyna@itrexgroup.com>
2020-12-09 13:56:47 +0200

    delete comments

--- framework/pages/tables/index.js
+++ framework/pages/tables/index.js
@@ -4,10 +4,9 @@ class TablesPage{
     constructor(page) {
         this.page = page
     }
-    
+
     async getPageHeaderTitleContent(){
         await waits(this.page).waitVisibility('.header h3');
-        // await this.page.waitForSelector('.header h3', {state: "visible"});
         const elementHandle = await this.page.$('.header h3');
         return elementHandle.textContent();
     }
